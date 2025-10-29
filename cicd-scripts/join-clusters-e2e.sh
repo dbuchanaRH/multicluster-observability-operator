314f0fb48348b48c60f5ffbfca8208651ef832f7
Daniel Buchanan <dbuchana@redhat.com>
2025-10-29 16:12:54 -0400

    Passing CA in join script
    Signed-off-by: Daniel Buchanan <dbuchana@redhat.com>

--- cicd-scripts/join-clusters-e2e.sh
+++ cicd-scripts/join-clusters-e2e.sh
@@ -1,39 +1,62 @@
 #!/bin/bash
 
+# can get hub cluster context
+export KUBECONFIG="${SHARED_DIR}/hub-1.kc"
+export CTX_HUB_CLUSTER=$(kubectl config current-context)
 # Extract hub cluster info from JSON
 HUB_API_URL=$(jq -r '.api_url' "${SHARED_DIR}/hub-1.json") &>/dev/null
-HUB_USER=$(jq -r '.username' "${SHARED_DIR}/hub-1.json")
-HUB_PASS=$(jq -r '.password' "${SHARED_DIR}/hub-1.json")
-echo "HUB_API_URL: ${HUB_API_URL}"
-clusteradm init &>/dev/null
-HUB_TOKEN=$(clusteradm get token) &>/dev/null
+TEMP_CA_FILE=$(mktemp)
+
+# Writing hub's CA to a temp file
+$(kubectl get cm kube-root-ca.crt -n kube-public -o jsonpath='{.data.ca\.crt}' > ${TEMP_CA_FILE})
+
+clusteradm init --wait --context ${CTX_HUB_CLUSTER} &>/dev/null
+HUB_TOKEN=$(clusteradm get token --context ${CTX_HUB_CLUSTER})
+
+if [[ -z "${HUB_TOKEN}" ]]; then
+    echo "Error: Failed to get hub token...\nSkipping join..."
+    # should not try to join if failed to get hub token...
+fi
 # list of managed clusters
 managed_clusters=""
 for ((i=1 ; i <= CLUSTERPOOL_MANAGED_COUNT ; i++)); do
   if [[ ! -z "${HUB_TOKEN}" ]] && [[ ! -z "${SHARED_DIR}" ]] && [[ -f "${SHARED_DIR}/managed-${i}.json" ]]; then
-    echo "Joining cluster ${i}"
     if [[ $i -eq 1 ]]; then
       managed_clusters="managed-${i}"
     else
       managed_clusters+=",managed-${i}"
     fi
-    MANAGED_CLUSTER_API_URL=$(jq -r '.api_url' "${SHARED_DIR}/managed-${i}.json" )
-    MANAGED_CLUSTER_USER=$(jq -r '.username' "${SHARED_DIR}/managed-${i}.json" )
-    MANAGED_CLUSTER_PASS=$(jq -r '.password' "${SHARED_DIR}/managed-${i}.json" )
-    echo "MANAGED_CLUSTER_API_URL: ${MANAGED_CLUSTER_API_URL}"
+    export KUBECONFIG="${SHARED_DIR}/managed-${i}.kc" 
+    export CTX_MANAGED_CLUSTER=$(kubectl config current-context)
 
-    oc login -u "$MANAGED_CLUSTER_USER" -p "$MANAGED_CLUSTER_PASS" "$MANAGED_CLUSTER_API_URL" # &>/dev/null
+    echo "Join managed-${i} to hub"
 
-    # need to check if this is the correct CA file
-    clusteradm join --hub-token ${HUB_TOKEN} --hub-apiserver ${HUB_API_URL} --cluster-name managed-${i} --ca-file /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --wait # &>/dev/null
+    clusteradm join \
+        --hub-token ${HUB_TOKEN} \
+        --hub-apiserver ${HUB_API_URL} \
+        --wait \
+        --cluster-name "managed-${i}" \
+        --context ${CTX_MANAGED_CLUSTER} \
+        --ca-file "$TEMP_CA_FILE"
   fi
 done
-echo "Waiting for klusterlet registration agent..."
-timeout 300 bash -c "until kubectl -n open-cluster-management-agent get pods | grep -q klusterlet-registration-agent.*Running; do sleep 10; done"
 
-# Wait for klusterlet work agent to be ready
-echo "Waiting for klusterlet work agent..."
-timeout 300 bash -c "until kubectl -n open-cluster-management-agent get pods | grep -q klusterlet-work-agent.*Running; do sleep 10; done"
-oc login -u "$HUB_USER" -p "$HUB_PASS" "$HUB_API_URL" # &>/dev/null
+kubectl config use ${CTX_HUB_CLUSTER}
+
 # Need to wait brefore accepting. Wait for each join to process and need timeout for each join.
-clusteradm accept --clusters ${managed_clusters} --wait
+echo "Waiting for CSRs to appear..."
+
+# TODO need to wait for CSR to be created for each managed cluster
+# for ((i=1 ; i <= CLUSTERPOOL_MANAGED_COUNT ; i++)); do
+#   if [[ ! -z "${HUB_TOKEN}" ]] && [[ ! -z "${SHARED_DIR}" ]] && [[ -f "${SHARED_DIR}/managed-${i}.json" ]]; then
+
+    
+#   fi
+# done
+clusteradm accept --clusters $managed_clusters --context ${CTX_HUB_CLUSTER}
+# can move accept logic down here
+# Checking for agent that runs on the managed cluster
+kubectl -n open-cluster-management-agent get pod --context ${CTX_MANAGED_CLUSTER}
+
+# Checking for agent that runs on the hub cluster
+kubectl get managedcluster --context ${CTX_HUB_CLUSTER}
