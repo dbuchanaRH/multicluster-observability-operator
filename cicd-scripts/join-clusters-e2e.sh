474a3a90e2d450aa930d9d6f29896937f9504539
Daniel Buchanan <dbuchana@redhat.com>
2025-11-03 11:19:32 -0500

    Hiding token from being printed by xtrace
    Signed-off-by: Daniel Buchanan <dbuchana@redhat.com>

--- cicd-scripts/join-clusters-e2e.sh
+++ cicd-scripts/join-clusters-e2e.sh
@@ -10,23 +10,28 @@ TEMP_CA_FILE=$(mktemp)
 kubectl get cm kube-root-ca.crt -n kube-public -o jsonpath='{.data.ca\.crt}' >${TEMP_CA_FILE}
 
 clusteradm init --wait --context ${CTX_HUB_CLUSTER} &>/dev/null
+set +x
 HUB_TOKEN=$(clusteradm get token --context ${CTX_HUB_CLUSTER})
 
+IS_HUB_TOKEN=true
 if [[ -z ${HUB_TOKEN} ]]; then
   echo "Error: Failed to get hub token..."
   echo "Skipping join..."
+  IS_HUB_TOKEN=false
 fi
+set -x
 
 declare -A CSR_CREATED=()
 
 for ((i = 1; i <= CLUSTERPOOL_MANAGED_COUNT; i++)); do
-  if [[ -n ${HUB_TOKEN} ]] && [[ -n ${SHARED_DIR} ]] && [[ -f "${SHARED_DIR}/managed-${i}.json" ]]; then
+  if $IS_HUB_TOKEN && [[ -n ${SHARED_DIR} ]] && [[ -f "${SHARED_DIR}/managed-${i}.json" ]]; then
 
     export KUBECONFIG="${SHARED_DIR}/managed-${i}.kc"
     export CTX_MANAGED_CLUSTER=$(kubectl config current-context)
 
     echo "Join managed-${i} to hub"
 
+    set +x
     clusteradm join \
       --hub-token ${HUB_TOKEN} \
       --hub-apiserver ${HUB_API_URL} \
@@ -34,6 +39,7 @@ for ((i = 1; i <= CLUSTERPOOL_MANAGED_COUNT; i++)); do
       --cluster-name "managed-${i}" \
       --context ${CTX_MANAGED_CLUSTER} \
       --ca-file "$TEMP_CA_FILE"
+    set -x
     CSR_CREATED[$i]=$i
   fi
 done
@@ -44,7 +50,7 @@ export KUBECONFIG="${SHARED_DIR}/hub-1.kc"
 echo "Waiting for CSR from each managed cluster..."
 for j in {1..60}; do
   for ((i = 1; i <= CLUSTERPOOL_MANAGED_COUNT; i++)); do
-    if [[ -n ${HUB_TOKEN} ]] && [[ -n ${CSR_CREATED[$i]} ]] && [[ $(kubectl get csr --context ${CTX_HUB_CLUSTER} 2>/dev/null | grep managed-${i}) ]]; then
+    if $IS_HUB_TOKEN && [[ -n ${CSR_CREATED[$i]} ]] && [[ $(kubectl get csr --context ${CTX_HUB_CLUSTER} 2>/dev/null | grep managed-${i}) ]]; then
       unset CSR_CREATED[$i]
       clusteradm accept --clusters "managed-${i}" --context ${CTX_HUB_CLUSTER} &>/dev/null
     fi
