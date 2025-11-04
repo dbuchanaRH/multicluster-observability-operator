6a24cc5e07705cf926719be2226b392ffbde744f
Daniel Buchanan <dbuchana@redhat.com>
2025-11-04 12:02:32 -0500

    Fixing format for join script
    Signed-off-by: Daniel Buchanan <dbuchana@redhat.com>

--- cicd-scripts/join-clusters-e2e.sh
+++ cicd-scripts/join-clusters-e2e.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-export KUBECONFIG="${SHARED_DIR}/hub-1.kc"
-export CTX_HUB_CLUSTER=$(kubectl config current-context)
+KUBECONFIG="${SHARED_DIR}/hub-1.kc"
+export KUBECONFIG
+CTX_HUB_CLUSTER=$(kubectl config current-context)
 # Extract hub cluster info from JSON
 HUB_API_URL=$(jq -r '.api_url' "${SHARED_DIR}/hub-1.json") &>/dev/null
 TEMP_CA_FILE=$(mktemp)
 
 # Writing hub's CA to a temp file
-kubectl get cm kube-root-ca.crt -n kube-public -o jsonpath='{.data.ca\.crt}' >${TEMP_CA_FILE}
+kubectl get cm kube-root-ca.crt -n kube-public -o jsonpath='{.data.ca\.crt}' >"${TEMP_CA_FILE}"
 
-clusteradm init --wait --context ${CTX_HUB_CLUSTER} &>/dev/null
+clusteradm init --wait --context "${CTX_HUB_CLUSTER}" &>/dev/null
 set +x
-HUB_TOKEN=$(clusteradm get token --context ${CTX_HUB_CLUSTER})
+HUB_TOKEN=$(clusteradm get token --context "${CTX_HUB_CLUSTER}")
 
 IS_HUB_TOKEN=true
 if [[ -z ${HUB_TOKEN} ]]; then
@@ -27,17 +28,17 @@ for ((i = 1; i <= CLUSTERPOOL_MANAGED_COUNT; i++)); do
   if $IS_HUB_TOKEN && [[ -n ${SHARED_DIR} ]] && [[ -f "${SHARED_DIR}/managed-${i}.json" ]]; then
 
     export KUBECONFIG="${SHARED_DIR}/managed-${i}.kc"
-    export CTX_MANAGED_CLUSTER=$(kubectl config current-context)
+    CTX_MANAGED_CLUSTER=$(kubectl config current-context)
 
     echo "Join managed-${i} to hub"
 
     set +x
     clusteradm join \
-      --hub-token ${HUB_TOKEN} \
-      --hub-apiserver ${HUB_API_URL} \
+      --hub-token "${HUB_TOKEN}" \
+      --hub-apiserver "${HUB_API_URL}" \
       --wait \
       --cluster-name "managed-${i}" \
-      --context ${CTX_MANAGED_CLUSTER} \
+      --context "${CTX_MANAGED_CLUSTER}" \
       --ca-file "$TEMP_CA_FILE"
     set -x
     CSR_CREATED[$i]=$i
@@ -50,16 +51,16 @@ export KUBECONFIG="${SHARED_DIR}/hub-1.kc"
 echo "Waiting for CSR from each managed cluster..."
 for j in {1..60}; do
   for ((i = 1; i <= CLUSTERPOOL_MANAGED_COUNT; i++)); do
-    if $IS_HUB_TOKEN && [[ -n ${CSR_CREATED[$i]} ]] && [[ $(kubectl get csr --context ${CTX_HUB_CLUSTER} 2>/dev/null | grep managed-${i}) ]]; then
-      unset CSR_CREATED[$i]
-      clusteradm accept --clusters "managed-${i}" --context ${CTX_HUB_CLUSTER} &>/dev/null
+    if $IS_HUB_TOKEN && [[ -n ${CSR_CREATED[$i]} ]] && kubectl get csr --context "${CTX_HUB_CLUSTER}" 2>/dev/null | grep -q "managed-${i}"; then
+      unset "CSR_CREATED[$i]"
+      clusteradm accept --clusters "managed-${i}" --context "${CTX_HUB_CLUSTER}" &>/dev/null
     fi
     if [[ ${#CSR_CREATED[@]} -eq 0 ]]; then
       break 2
     fi
   done
   if [[ ${j} -eq 60 ]]; then
-    for managed_num in ${!CSR_CREATED[@]}; do
+    for managed_num in "${!CSR_CREATED[@]}"; do
       echo "timeout wait for CSR for managed-${managed_num} is not created."
     done
     break
@@ -69,4 +70,4 @@ for j in {1..60}; do
 done
 
 # Checking that managed clusters were successfully added
-kubectl get managedcluster --context ${CTX_HUB_CLUSTER}
+kubectl get managedcluster --context "${CTX_HUB_CLUSTER}"
